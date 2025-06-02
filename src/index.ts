37fdee5691bf98d9e158c3daac9f199da5c41d9b
Garv Thakral <garvthakralfx@gmail.com>
2025-06-02 18:51:06 +0530

    deployment fixes

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use("/user",userRouter)
+app.get("/",async(req,res)=>{
+    res.json({
+        message:"Seems to be working"
+    })
+})
 app.listen(3000)
